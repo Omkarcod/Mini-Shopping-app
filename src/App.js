0b931bc1b706b742131592aa2d663767473bbb16
Omkarcod <132255399+Omkarcod@users.noreply.github.com>
2024-02-15 14:39:29 +0530

    shopping app

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import "./App.css";
+import data from "./components/back/Data/Data";
+import Header from "./components/front/header/Header";
+import CartRoutes from "./components/front/cartRoutes/CartRoutes";
+import { BrowserRouter as Router } from "react-router-dom";
+import { useState } from "react";
+
+function App() {
+  const { productItems } = data;
+  const [cartItems, setCartItems] = useState([]);
+
+  const handleAddProduct = (product) => {
+    const ProductExist = cartItems.find((item) => item.id === product.id);
+    if (ProductExist) {
+      setCartItems(
+        cartItems.map((item) =>
+          item.id === product.id
+            ? { ...ProductExist, quantity: ProductExist.quantity + 1 }
+            : item
+        )
+      );
+    } else {
+      setCartItems([...cartItems, { ...product, quantity: 1 }]);
+    }
+  };
+
+  const handleRemoveProduct = (product) => {
+    const ProductExist = cartItems.find((item) => item.id === product.id);
+    if (ProductExist.quantity === 1) {
+      setCartItems(cartItems.filter((item) => item.id !== product.id));
+    } else {
+      setCartItems(
+        cartItems.map((item) =>
+          item.id === product.id
+            ? { ...ProductExist, quantity: ProductExist.quantity - 1 }
+            : item
+        )
+      );
+    }
+  };
+
+   const handleCartClearance=()=>{
+    setCartItems([])
+   }
+
+  return (
+    <div>
+      <Router>
+        <Header cartItems={cartItems}/>
+        <CartRoutes
+          productItems={productItems}
+          cartItems={cartItems}
+          handleAddProduct={handleAddProduct}
+          handleRemoveProduct={handleRemoveProduct}
+          handleCartClearance={handleCartClearance}
+        />
+      </Router>
+    </div>
+  );
+}
+
+export default App;
